61de1f54401c470e1c88f8a9acc00287f8e4122e
Sylvain DNS <16577439+sylvainDNS@users.noreply.github.com>
2022-11-01 00:38:00 +0100

    build: Setup deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  base: '/10-idees-recues-anarchisme',
+})
