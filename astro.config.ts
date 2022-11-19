ed3833279172a5e2215ec471c518a6052cf9a6dc
Sylvain DENYSE <sylvain.denyse@gmail.com>
2022-11-19 01:01:57 +0100

    build: minify html & inline js

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 import mdx from '@astrojs/mdx'
@@ -6,5 +7,6 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   base: '/10-idees-recues-anarchisme',
-  integrations: [mdx()],
+  integrations: [mdx(), compress()],
+  vite: {},
 })
